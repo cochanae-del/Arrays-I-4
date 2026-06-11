5cef6dbb237f427b22128a38221b2dafff74db86
cochanae-del <co.chanae@gmail.com>
2026-06-11 20:01:52 +0000

    Amend the array with corrections using only the splice method.

--- main.js
+++ main.js
@@ -2,5 +2,8 @@
 var ranks = ["domain", "kingdom", "category", "family", "genus"];
 
 // Write your code below
-ranks.splice(2,2, "phylum", "class", "order", "species");
+ranks.splice(2,1, "phylum", "class", "order",);
+// add 3 strings, remove string "category".
+// add "species" to end of the array
+ranks.splice(7,0, "species");
 console.log(ranks);
